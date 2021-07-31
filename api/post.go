01b3feebd3e547d1022863c22e935766f9007bec
CM-IV <cmathIV@protonmail.com>
2021-07-30 21:57:33 -0500

    Max number of results on page changed.

--- api/post.go
+++ api/post.go
@@ -21,7 +21,7 @@ type getPostRequest struct {
 
 type listPostRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=15"`
 }
 
 type updatePostRequest struct {
